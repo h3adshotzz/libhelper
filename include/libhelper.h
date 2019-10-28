008b5b3aefb40c5c033f5068d5ee22f3c0bb2cfc
Harry Moulton <h3adsh0tzz@Harrys-MacBook-Pro.local>
2019-10-28 10:43:45 +0000

    nothing major

--- include/libhelper.h
+++ include/libhelper.h
@@ -1,1 +1,27 @@
-void libhelper_test ();
+/**
+ *     libhelper
+ *     Copyright (C) 2019, @h3adsh0tzz
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+#ifndef LIBHELPER_H
+#define LIBHELPER_H
+
+#include <stdio.h>
+
+char *libhelper_version_string ();
+
+#endif /* libhelper_h */
