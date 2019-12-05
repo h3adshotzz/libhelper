ad4530e36d87ac997bf7e3214bd0d53bde1084a8
h3adsh0tzz <me@h3adsh0tzz.com>
2019-12-05 20:00:28 +0000

    Added docs to macho-dyld.h

--- include/macho/macho-dyld.h
+++ include/macho/macho-dyld.h
@@ -49,7 +49,42 @@ typedef struct dyld_rebase_info {
 
 
 /**
+ *  [Notes]
  * 
+ *  These are my notes for the Dynamic Loader info.
+ * 
+ *  The rebase info, for example, has the following hex value.
+ * 
+ *      11 23 00 60 10 00 00 00
+ * 
+ *  But the following translated value (opcodes).
+ * 
+ *      11      REBASE_OPCODE_SET_TYPE_IMM                      type (1, REBASE_TYPE_POINTER)
+ *      23      REBASE_OPCODE_SET_SEGMENT_AND_OFFSET_ULEB       segment (3)
+ *      00      uleb128                                         offset (0)
+ *      60      REBASE_OPCODE_DO_REBASE_ULEB_TIMES              ...
+ *      10      uleb128                                         count (16)
+ *      -------
+ *      00      REBASE_OPCODE_DONE
+ * 
+ *  And the actions list looks like:
+ *      
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003000     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003008     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003010     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003018     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003020     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003028     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003030     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003038     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003040     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003048     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003050     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003058     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003060     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003068     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003070     Pointer
+ *      00004000    ...     __DATA __la_symbol_ptr      0x100003078     Pointer   
  * 
  */
 
